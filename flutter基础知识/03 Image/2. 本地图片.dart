678dd567ae6c16f6af418bee2991f385ad778ce2
重王泉 <1813114234@qq.com>
2020-02-17 15:35:36 +0800

    flutter基础知识及apk打包

---
+++ flutter基础知识/03 Image/2. 本地图片.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'good',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(  // 布局组件
+        body:  Center(  // body组件
+          child: Container(
+              child: new Image.asset(
+                'image/sell-out.png',
+                fit: BoxFit.scaleDown,
+                color: Colors.orange,
+                colorBlendMode: BlendMode.darken,
+                repeat: ImageRepeat.repeatY,
+              ),
+              width: 300.0,
+              height: 400.0,
+              color: Colors.blue,
+              // color: Colors.yellow,
+            )
+          )
+        )
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,45 @@
 import 'package:flutter/material.dart';
-
 void main () {
   runApp(MaterialApp(
-    title: "页面跳转返回数据",
-    home: ProductList()
+    // title: '我是第一屏',
+    home: new FirstScreen()
   ));
 }
+  
 
-class ProductList extends StatelessWidget{
-  @override
-  Widget build(BuildContext context ){
-      return Scaffold(
-          appBar: AppBar(title: new Text('找人工具')),
-          body: Center(child: RouteButton(),)
-      );
-  }
-}
-
-class RouteButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {
-        _navigateToThis(context);
-      },
-      child: Text('去找人')
-    );
-  }
-
-  _navigateToThis(BuildContext context) async{
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context)=>FindPerson()
-    ));
-    Scaffold.of(context).showSnackBar((SnackBar(content: Text('$result'))));
-  }
-}
-class FindPerson extends StatelessWidget {
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(title: Text('我是你要找的人')),
-      body: Center(
-        child: Column(children: <Widget>[
-          RaisedButton(child: Text('第一个人'),onPressed:(){
-            Navigator.pop(context, '第一个人，18557525812');
-          }),
-          RaisedButton(child: Text('第二个人'),onPressed:(){
-            Navigator.pop(context, '第二个人，33333333');
-          })
-        ],)
+      appBar: AppBar(title: Text('第一屏')),
+      body: Center(child: RaisedButton(
+        child: Text('查看商品详情'),
+        onPressed: (){
+          Navigator.push(
+            context, 
+            new MaterialPageRoute(
+              builder: (context) => new SecondScreen()
+            )
+          );
+        },
+        ),
+      )
+    );
+  }
+}
+
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(title: Text('第二屏')),
+      body: Center(child: RaisedButton(
+        child: Text('我是子页-返回'),
+        onPressed: (){
+          Navigator.pop(context);
+        },
+        ),
       )
     );
   }
